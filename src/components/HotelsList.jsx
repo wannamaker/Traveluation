52b8207ee02b940f22c0d074db4c188fa67c8734
wannamaker <wannamakerht@gmail.com>
2020-06-03 21:23:27 -0400

    displaying much data for an item

--- src/components/HotelsList.jsx
+++ src/components/HotelsList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../css/HotelsList.css';
 
 export default function HotelsList(props) {
   return (
@@ -12,9 +13,35 @@ export default function HotelsList(props) {
                        <img src={`https://photo.hotellook.com/image_v2/limit/h${hotel.hotelId}_1/160.jpg`}/>
                 </div>
         
-                  <div className="hotel-info">
-                       <h3>{hotel.hotelName}</h3>
+            <div className="hotel-info">
+                              <div>
+                               <h3>{hotel.hotelName}</h3>
+                               <p>{hotel.stars} stars</p>
+                              </div>
+                               <div className="detail-container">
+                <div className="price-display">
+                <br />
+                  <p className="p-price">Total for N days:</p> <br />
+                  <p></p>
+                  <br />
+                  <p>Price per night:</p> <br />
+                  <p></p>
+                </div>
+                                     <div className="detail-info"></div>
+                <div className="location">
+                  <br />
+                  <p>Country: {hotel.location.country}</p>
+                  <br />
+                  <p>State: {hotel.location.state}</p>
+                </div>
+                              </div>
                   </div>
+                  
+            
+                  <div className="map">
+              
+                  </div>
+
             </div>
         </Link>
       )}
